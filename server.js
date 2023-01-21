f593b3d6dc707c09bc72b78a4ea55ea609420f2e
Sangsaka Wira <sangsakawira@gmail.com>
2023-01-21 11:48:11 +0700

    iot example

---
+++ server.js
@@ -0,0 +1,42 @@
+// 1. buat folder project
+// 2. ketik "npm init" dienter sampai keluar package.json
+
+const express = require('express')
+const app = express()
+let port = process.env.PORT || 8080
+
+let data = {
+    temperature:"0",
+    pressure:"0",
+    humidity:"0"
+}
+
+app.get('/get-data', function (req, res) {
+  res.send(data)
+})
+
+app.get('/set-temperature/:temperature', function (req, res) {
+    data = {
+        ...data,
+        temperature:req.params.temperature
+      }
+    res.send(data)
+  })
+
+  app.get('/set-pressure/:pressure', function (req, res) {
+    data = {
+        ...data,
+        pressure:req.params.pressure
+      }
+    res.send(data)
+  })
+
+  app.get('/set-humidity/:humidity', function (req, res) {
+    data = {
+        ...data,
+        humidity:req.params.humidity
+      }
+    res.send(data)
+  })
+
+app.listen(port)
